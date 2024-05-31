5f0aeb08fb8b0457f402776baee69b0234dff187
EdwinBayingana <bayinganaedwin@gmail.com>
2024-05-31 06:29:46 -0400

    initial commit

---
+++ TestProjSwift/TestProjSwift.swift
@@ -0,0 +1,17 @@
+//
+//  InvisibleTasksSwiftApp.swift
+//  InvisibleTasksSwift
+//
+//  Created by Edwin Bayingana on 31/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct InvisibleTasksSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
